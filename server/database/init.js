275e78bbefadb14ac7ccec22eb0985ff24c5ea67
sharma12lalit <sharma12lalit@gmail.com>
2025-06-28 12:17:03 +0000

    t

--- server/database/init.js
+++ server/database/init.js
@@ -40,7 +40,8 @@ export const initDatabase = async () => {
         identifier TEXT NOT NULL, -- Can be email or mobile number
         otp TEXT NOT NULL,
         expires_at INTEGER NOT NULL -- Unix timestamp
-      )
+ ,
+        created_at INTEGER NOT NULL
     `);
 
 
