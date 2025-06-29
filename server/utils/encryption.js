960c1cb70155e07aa08d4955000d7eae31671f3d
lalit4389 <40782322+lalit4389@users.noreply.github.com>
2025-06-29 21:44:33 +0530

    Fix encryption cipher initialization errors

--- server/utils/encryption.js
+++ server/utils/encryption.js
@@ -31,7 +31,7 @@ export const encryptData = (text) => {
 
     const key = getKey();
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipher('aes-256-cbc', key);
+    const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
     
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
@@ -58,7 +58,7 @@ export const decryptData = (encryptedData) => {
     const iv = Buffer.from(parts[0], 'hex');
     const encrypted = parts[1];
     
-    const decipher = crypto.createDecipher('aes-256-cbc', key);
+    const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     
